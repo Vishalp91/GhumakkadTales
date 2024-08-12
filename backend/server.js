23890d6d0e0e45270f77f7b563f03f7c7b4093c1
Vishal Pawar <vishalpawar.latur@gmail.com>
2024-08-12 17:38:42 +0530

    Client Configured

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ import 'dotenv/config.js';
 import { postRoutes } from './routes/postRoutes.js';
 import { userRoutes } from './routes/userRoutes.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolving dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +17,12 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
+
+//Use Frontend App
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+//Render Client for any path 
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/dist/index.html')));
 
 main()
 .then(() => {
